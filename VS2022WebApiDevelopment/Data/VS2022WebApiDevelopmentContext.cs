92cb1958863366e5130d89711f944bd7e0e4b3fc
Yuta Matsumura <tkmbmbr@gmail.com>
2023-05-22 23:41:53 +0900

    Update db context

--- VS2022WebApiDevelopment/Data/VS2022WebApiDevelopmentContext.cs
+++ VS2022WebApiDevelopment/Data/VS2022WebApiDevelopmentContext.cs
@@ -1,19 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using VS2022WebApiDevelopment;
+﻿using Microsoft.EntityFrameworkCore;
 
-namespace VS2022WebApiDevelopment.Data
+namespace VS2022WebApiDevelopment.Data;
+
+public class VS2022WebApiDevelopmentContext : DbContext
 {
-    public class VS2022WebApiDevelopmentContext : DbContext
+    public VS2022WebApiDevelopmentContext(DbContextOptions<VS2022WebApiDevelopmentContext> options)
+        : base(options)
     {
-        public VS2022WebApiDevelopmentContext (DbContextOptions<VS2022WebApiDevelopmentContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<VS2022WebApiDevelopment.Contact> Contact { get; set; } = default!;
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Contact>().HasData(GetSeedDataContacts());
+    }
+
+    public DbSet<Contact> Contact { get; set; } = default!;
+
+    private Contact[] GetSeedDataContacts() => new Contact[] {
+        new Contact {
+            Id = 1,
+            Name = "Sayed Hashimi",
+            Email = "sayed@example.com",
+            Phone = "555-111-2222"
+        },
+        new Contact {
+            Id=2,
+            Name = "Mads Kristensen",
+            Email = "mads@example.com",
+            Phone = "555-111-3333"
+        },
+        new Contact {
+            Id=3,
+            Name = "Eline Barstad",
+            Email = "elineb@example.com",
+            Phone = "555-111-4444"
+        },
+        new Contact {
+            Id=4,
+            Name = "Theodore Lamy",
+            Email = "theol@example.com",
+            Phone = "555-111-5555"
+        },
+        new Contact {
+            Id=5,
+            Name = "María Zelaya",
+            Email = "mariaz@example.com",
+            Phone = "555-111-6666"
+        },
+        new Contact {
+            Id=6,
+            Name = "Kubanychbek Sagynbek",
+            Email = "kubans@example.com",
+            Phone = "555-111-7777"
+        },
+        new Contact {
+            Id=7,
+            Name = "Denise Bourgeois",
+            Email = "deniseb@example.com",
+            Phone = "555-111-8888"
+        },
+        new Contact {
+            Id=8,
+            Name = "Robin Danielsen",
+            Email = "robind@example.com",
+            Phone = "555-111-9999"
+        }
+    };
 }
